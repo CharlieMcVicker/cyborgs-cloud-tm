e93430ba0b8ab11594c0ca80c3021a3878e7a644
Charlie McVicker <charlie@mcvickernet.com>
2023-11-10 09:32:25 -0500

    clean up client code

--- cloud_tm_client/cloud_tm_client.ino
+++ cloud_tm_client/cloud_tm_client.ino
@@ -5,67 +5,87 @@
 #include <AccelStepper.h> //Include AccelStepper library, via Arduino or Internet
 
 // stepper definitions
-#define DIR 7                   //DIR pin from A4988 to pin 7
-#define STEP 4                  //STEP pin from A4988 to pin 4
-#define MOTOR_INTERFACE_TYPE 1  //How many motors are connected (Maximum motors are 4)
-byte numSteps = 0;              // The number of steps to take.
-int currentSteps = 0;           // The current number of steps taken.
-#define MAX_STEPS 100            // This is the maximum number of steps that will advance (You can modify this value).
+#define DIR 7                  // DIR pin from A4988 to pin 7
+#define STEP 4                 // STEP pin from A4988 to pin 4
+#define MOTOR_INTERFACE_TYPE 1 // How many motors are connected (Maximum motors are 4)
+byte numSteps = 0;             // The number of steps to take.
+int currentSteps = 0;          // The current number of steps taken.
+#define MAX_STEPS 100          // This is the maximum number of steps that will advance (You can modify this value).
 #define FULLSTEP 4
-#define STEP_PER_REVOLUTION 2048 // this value is from datasheet
+// I think maybe there are only 200 steps per rotation... the "step angle" is
+// listed as 1.80º
+#define STEP_PER_REVOLUTION 2048 // this value is from datasheet -- this comment is from john
 
-AccelStepper stepper = AccelStepper(MOTOR_INTERFACE_TYPE, STEP, DIR); //Create a new instance of the AccelStepper class
-
-int led = 13; // Pin 13
+AccelStepper stepper = AccelStepper(MOTOR_INTERFACE_TYPE, STEP, DIR); // Create a new instance of the AccelStepper class
 
 void setup()
 {
-  pinMode(led, OUTPUT); // Set pin 13 as digital out
-
   // Start serial connection
   Serial.begin(9600);
   Serial.flush();
 
+  // Setup the motor
   stepper.setMaxSpeed(1000.0);   // set the maximum speed
   stepper.setAcceleration(50.0); // set acceleration
   stepper.setSpeed(200);         // set initial speed
   stepper.setCurrentPosition(0); // set position
-  stepper.moveTo(STEP_PER_REVOLUTION); // set target position: 64 steps <=> one revolution
+  // stepper.moveTo(STEP_PER_REVOLUTION); // set target position: 64 steps <=> one revolution
 }
+
+enum State
+{
+  WaitingForCommand,
+  Opening,
+  Closing,
+}
+
+State state = State.WaitingForCommmand;
+String command = "";
 
 void loop()
 {
-  String input = "";
 
-  // Read any serial input
-  while (Serial.available() > 0)
+  switch (state)
   {
-    input += (char)Serial.read(); // Read in one char at a time
-    delay(5);                     // Delay for 5 ms so the next char has time to be received
+  case State.WaitingForCommand:
+    command = Serial.readStringUntil("\n");
+    if (command == "OPEN_VALVE")
+    {
+      serial.println("opening");
+      state = State.Opening;
+      stepper.moveTo(STEPS_PER_REVOLUTION / 4);
+    }
+    else if (command == "CLOSE_VALVE")
+    {
+      serial.println("closing");
+      state = State.Closing;
+      stepper.moveTo(0);
+    }
+  case State.Opening:
+  case State.Closing:
+    runMotor();
   }
+}
 
-  if (input == "on")
+void runMotor()
+{
+  stepper.run();
+
+  if (stepper.distanceToGo() == 0)
   {
-    if (digitalRead(13) == HIGH)
+    // mot sure what this does? maybe keeps it spinning?
+    // stepper.moveTo(stepper.currentPosition());
+
+    switch (state)
     {
-      Serial.write("Led is already ON \n");
+    case State.Closing:
+      serial.println("closed");
+      break;
+    case State.Opening:
+      serial.println("opened");
+      break;
     }
-    else
-    {
-      digitalWrite(led, HIGH); // on
-      Serial.write("LED is on \n");
-    }
-  }
-  else if (input == "off")
-  {
-    if (digitalRead(13) == LOW)
-    {
-      Serial.write("Led is already OFF \n");
-    }
-    else
-    {
-      digitalWrite(led, LOW); // on
-      Serial.write("LED is off \n");
-    }
+
+    state = State.WaitingForCommand;
   }
 }
